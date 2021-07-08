b78edc5d1812c5103f7298dc47ba675ba22e6d17
NaNathan13 <natewilson135@gmail.com>
2021-07-08 17:55:05 -0400

    removed rbg components

--- template-parts/components/discord_cta.php
+++
@@ -1,5 +0,0 @@
-<section class="discord_cta_sidebar_section">
-    <div class="discord_cta_sidebar_container">
-        <a class="cta_btn" href="<?php echo get_field('discord_url', 'option') ?>">Join Our Discord</a>
-    </div>
-</section>
--- template-parts/components/twitch_cta.php
+++
@@ -1,5 +0,0 @@
-<section class="twitch_cta_sidebar_section">
-    <div class="twitch_cta_sidebar_container">
-        <a class="cta_btn" href="<?php echo get_field('twitch_url', 'option') ?>">Watch Us on Twitch</a>
-    </div>
-</section>
